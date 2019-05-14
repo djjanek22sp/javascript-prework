6eeec09b81624efd1aaf6788618429a75846ad9f
Daniel Janczyło <d.janczylo@gmail.com>
2019-05-14 10:40:44 +0200

    Add if-else elements do script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove
 playerMove = 'papier'
 printmessage ('Zagrałem' + playerMove + '!Jeśli Twój ruch to papier, to wygrywasz!')
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
