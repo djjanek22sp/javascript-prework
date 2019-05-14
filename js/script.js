47fbf2ecaa48ea53a1c61b4c9be54fd695b21459
Daniel Janczyło <d.janczylo@gmail.com>
2019-05-14 10:43:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-var playerMove
-playerMove = 'papier'
-printmessage ('Zagrałem' + playerMove + '!Jeśli Twój ruch to papier, to wygrywasz!')
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
