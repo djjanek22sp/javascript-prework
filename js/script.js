8a2bc6138bf22682544843566235096aeebf1f9c
Daniel Janczyło <d.janczylo@gmail.com>
2019-05-14 10:51:07 +0200

    Add playerMove to script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@ if (randomNumber == '3') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '3') {
+  playerMove = 'chleb';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
