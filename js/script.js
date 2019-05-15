02aa4378b3f99f57882d575c00e850c4a75bb78b
Daniel Janczyło <d.janczylo@gmail.com>
2019-05-15 16:54:25 +0200

    Update js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,12 @@
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else {
-  computerMove = 'nieznany ruch';
-}
-if (randomNumber == '2') {
-  computerMove = 'papier';
-} else {
-  computerMove = 'nieznany ruch';
-}
-if (randomNumber == '3') {
-  computerMove = 'nożyce';
-} else {
-  computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch: ' + computerMove);
-var playerInput, playerMove;
+var playerMove, playerInput;
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
   playerMove = 'kamień';
-} else {
-  playerMove = 'nieznany ruch';
-}
-if (playerInput == '2') {
+} else if (playerInput == '2') {
   playerMove = 'papier';
-} else {
-  playerMove = 'nieznany ruch';
-}
-if (playerInput == '3') {
-  playerMove = 'chleb';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
 } else {
   playerMove = 'nieznany ruch';
 }
